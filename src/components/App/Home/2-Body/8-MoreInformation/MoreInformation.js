8d945fc10db07beccfc8af3e6e0a381a078a28b9
royexr <royexrc@gmail.com>
2019-10-15 10:51:17 -0500

    warnings and key corrections p2

--- src/components/App/Home/2-Body/8-MoreInformation/MoreInformation.js
+++ src/components/App/Home/2-Body/8-MoreInformation/MoreInformation.js
@@ -8,39 +8,33 @@ import './MoreInformation.css';
 
 
 export function MoreInformation(){
-    const _formInformations = [
-        {
-          type: 1,
-          title: 'MAS INFORMACIÓN',
-          subtitle:'E-mail'
-         
-        }
-      ];
-
-    return(
-            <section id="MoreInformation" className="moreInformation">
-                <div className="moreInformation_info">
-                    {_formInformations.map((form) =>
-                    < FormInformation
-                        key = {_formInformations.indexOf(form)}
-                        type = {_formInformations.type}
-                        title = {form.title}
-                        subtitle={form.subtitle}
-                    />
-                    )}
-
-                </div>
-                   
-
-            </section>
-
-        );
+  const _formInformations = [
+    {
+      type: 1,
+      title: 'MAS INFORMACIÓN',
+      subtitle:'E-mail'
     }
+  ];
+
+  return(
+    <section id="MoreInformation" className="moreInformation">
+      <div className="moreInformation_info">
+        {_formInformations.map((form) =>
+          <FormInformation
+            key = {_formInformations.indexOf(form)}
+            type = {form.type}
+            title = {form.title}
+            subtitle={form.subtitle}
+          />
+        )}
+      </div>
+    </section>
+  );
+}
 
 MoreInformation.propTypes = {
     type: PropTypes.number,
     serviceClass: PropTypes.string,
     title: PropTypes.string,
     subtitle:PropTypes.string
-   
-  }
+};
