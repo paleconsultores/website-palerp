d7c69d54cfd97de9be060628023ec0374b1834d1
nidhogg-spec <55214512+nidhogg-spec@users.noreply.github.com>
2019-10-16 10:45:25 -0500

    Revert "fix responsive bug carousel"
    This reverts commit 3ff3d6d1e0b785ad5a9e5bec829cdeb3cf44cf89.

--- src/components/App/Home/2-Body/4-Carousel/CarouselData/CarouselData.js
+++ src/components/App/Home/2-Body/4-Carousel/CarouselData/CarouselData.js
@@ -1,5 +1,5 @@
-import img1 from '../images/img1.jpg'
-import img2 from '../images/facturacionElectronica.jpg';
+import img1 from '../images/img11.jpg'
+import img2 from '../images/facturacionElectronica1.jpg';
 export const data = {
     "properties":[
         {
