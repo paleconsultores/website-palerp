c2a97247fedebaf4f6318188430e04c07cb1dc41
nidhogg-spec <55214512+nidhogg-spec@users.noreply.github.com>
2019-10-18 09:35:50 -0500

    Revert "Merge branch 'master' of https://github.com/paleconsultores/website-palerp"
    This reverts commit 758d77e24adc6aa51735d10d71d08d8a45de3ab6, reversing
    changes made to d5d5f4d98e41a0525ae1744bb892368ae0bfc421.

--- src/components/App/Home/1-Header/Header.js
+++ src/components/App/Home/1-Header/Header.js
@@ -1,41 +1,23 @@
 //Dependencies
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 //Resources
 import { BurgerMenu } from "./BurgerMenu";
 import { MenuList } from "./MenuList";
 
+
 import './Header.css'
-import logoPaleNegativo from '../../../../images/Logo_Pale_Negativo_Imagen.png';
-import logoPalePrincipal from '../../../../images/Logo_Pale_Principal_Imagen.png';
+import logoPale from '../../../../images/Logo_Pale_Negativo_Imagen.webp';
 
 export function Header() {
-  const remVal = parseFloat(getComputedStyle(document.documentElement).fontSize)
-
-  const [shouldBgChange, setShouldBgChanged] = useState(false);
-
-  useEffect(function () {
-    function onScroll() {
-      const headerMenu = document.getElementsByClassName("header__menu");
-
-      const changeBackground = window.scrollY > (window.innerHeight - 5.5 * remVal);
-
-      if (shouldBgChange !== changeBackground) {
-        setShouldBgChanged(changeBackground);
-      }
-    }
-
-    document.addEventListener("scroll", onScroll)
-  });
-
   return(
-    <header className={'header ' + (shouldBgChange ? 'header--positive' : '')}>
+    <header className="header">
       <nav className="header__navbar topnav">
         <a href="#Hero" className="header__brand">
-          <img className="header__brand-image" src={shouldBgChange && window.innerWidth > 1011 ? logoPalePrincipal : logoPaleNegativo} alt="logoPale" />
+          <img className="header__brand-image" src={logoPale} alt="logoPale" />
         </a>
         <BurgerMenu />
-        <div className={'header__menu ' + (shouldBgChange && window.innerWidth > 1011 ? 'header__menu--positive' : '')}>
+        <div className="header__menu">
           <MenuList />
         </div>
       </nav>
