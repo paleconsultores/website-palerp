7bf779414a4a20b2fcf7e01db6dd8f0ebdd5e633
BrendaQC <50086499+BrendaQC@users.noreply.github.com>
2019-10-17 19:11:21 -0500

    arreglos en el footer

--- src/components/App/Home/3-Footer/Footer.js
+++ src/components/App/Home/3-Footer/Footer.js
@@ -5,8 +5,9 @@ import React from 'react';
 import './styles.css'
 import logoPale from '../../../../images/Logo_Pale_Negativo_Imagen.png';
 import { faFacebookSquare,faWhatsappSquare, faYoutubeSquare} from "@fortawesome/free-brands-svg-icons";
+import { faHome, faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faPhone} from "@fortawesome/free-solid-svg-icons";
 export const Footer = () => {
   return(
     <footer>
@@ -14,16 +15,13 @@ export const Footer = () => {
       <div className="footer_info">
         <div>
           <img className="logo_Footer" src={logoPale} alt="logo_footer"></img>
+          <hr/>
+          <p>Desarrollamos soluciones a tu medida</p>
         </div>
-        <div>
-          <h5>Urb.Marcavalle E-15,Cusco-Cusco</h5>
-          <h5>Info@palerp.com</h5>
-          <h5>984-125-256</h5>
-        </div>
+        
         <div>
           <h5>Siguenos en:</h5>
             <div>
-
               <a  className="social" href="https://www.youtube.com/PaleConsultores">
                 <FontAwesomeIcon icon={faYoutubeSquare} size="3x" color="white"/>
               </a> 
@@ -36,11 +34,48 @@ export const Footer = () => {
               </a>
             </div>
         </div>
+
+        <div>
+            <h5>Servicios</h5>
+              <div className="footer_servicios">
+                  <div>
+                      <p>Aplicaciones Web</p>
+                      <p>Aplicaciones Móviles</p>
+                      <p>Internet de las Cosas</p>
+                  </div>
+                  <div>
+                        <p>Consultoría de Software</p>
+                        <p>Facturación electrónica</p>   
+                  </div>
+               </div>
+        </div>
       </div>
+
+      <div>
+         <hr/>
+      </div>
+      
+
+      <div className="footer_info">
+          <div>
+            <h4><FontAwesomeIcon icon={faPhone}/> 984-125-256</h4>
+            <p>9:00 AM - 6:00 PM</p>
+          </div>
+          <div>
+            <h4><FontAwesomeIcon icon={faMailBulk}/> Info@palerp.com</h4>
+            <p>Consultas On-line</p>
+          </div>
+
+          <div>
+              <h4><FontAwesomeIcon icon={faHome}/> Urb.Marcavalle E-20,Wanchaq</h4>
+              <p>Cusco-Perú</p>
+          </div>
+      </div>
+
       <div className="copyrigth">
-        <h1>Copyright © 2019 Pale Consultores</h1>
+        <p>Copyright © 2019Palerp</p>
       </div>
-      <div></div>
+     
     </footer>
   );
 }
