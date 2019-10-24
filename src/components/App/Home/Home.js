464ff1df87bba882df84023a4e1bcca87ea4060b
royexr <royexrc@gmail.com>
2019-10-24 09:32:08 -0500

    comments added and change class components to functions

--- src/components/App/Home/Home.js
+++ src/components/App/Home/Home.js
@@ -1,20 +1,18 @@
-//Dependencies
+// Dependencies
 import React from 'react';
 
-//Resources
+// Resources
 import { Header } from './1-Header/Header';
 import { Body } from './2-Body';
 import { Footer } from './3-Footer/Footer';
 import './Home.css'
 
-export class Home extends React.Component{
-  render() {
-    return(
-      <section className="Home">
-        <Header></Header>
-        <Body></Body>
-        <Footer></Footer>
-      </section>
-    );
-  }
+export function Home() {
+  return(
+    <section className="Home">
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </section>
+  );
 }
